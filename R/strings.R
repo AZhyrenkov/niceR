5dc7720ad325425cc6743df1200bb4d55c4ed534
azhyrenkov <ozhyrenkov@hotmail.com>
2018-07-24 09:06:31 +0300

    Cyrrilic encoding fixing function

--- R/strings.R
+++ R/strings.R
@@ -3,3 +3,9 @@
 
 # read sql query from file -----
 read_query <- function(path) suppressWarnings(paste(readLines(path),collapse = '\n'))
+
+# fix cyrrilic encoding
+fix_Enc_Cyr <- function(vector) {
+  if(is.character(vector)){iconv(x = vector,"UTF-8","CP1251")}
+  else{vector}
+}
